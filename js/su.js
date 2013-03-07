82d5795d126aed98aeeab1c0c01e4ce0289b9f1a
Adrian Bruinhout <adrian@webfella.com.au>
2013-03-07 16:26:09 +1000

    adding a helper to sortable to maintain table cell widths

--- js/su.js
+++ js/su.js
@@ -1,5 +1,13 @@
 jQuery(document).ready(function() {
 	jQuery('table.users tbody').sortable({
-		items: '> tr'
+		items: '> tr',
+		helper: function(e, tr) {
+			var originals = tr.children(),
+			helper = tr.clone();
+			helper.children().each(function(index) {
+				jQuery(this).width(originals.eq(index).width());
+			});
+			return helper;
+		}
 	});
 });
